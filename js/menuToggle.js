ac389aa5b9e5fa347eedc364e0f24fe05e6004b3
Igooha <igoooha@gmail.com>
2014-08-12 13:42:06 +0700

    Изменил название id провайдеров

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -2,15 +2,13 @@ $(document).ready(function(){
     $('.selectors__button').click(function(){
         $('.menu').toggleClass('_detailed');
 
-        if($('.menu').hasClass('_detailed') == false)
-        {
-            if($(this).attr('id') == 'provider_2')
-            {
-                var provider_1_value = $('#provider_1').html();
-                var provider_2_value = $('#provider_2').html();
+        if($('.menu').hasClass('_detailed') == false) {
+            if($(this).attr('id') == 'provider_2') {
+                var selected_provider_value = $('#main_provider').html();
+                var provider_value = $('#provider').html();
 
-                $('#provider_1').html(provider_2_value);
-                $('#provider_2').html(provider_1_value);
+                $('#main_provider').html(provider_value);
+                $('#provider').html(selected_provider_value);
             }
         }
     });
