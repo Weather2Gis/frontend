9da29d59b4c1ac757973124a9bd534c9d0d312c7
Igooha <igoooha@gmail.com>
2014-08-12 14:38:45 +0700

    Функция для получения данных от backend

---
+++ js/provider.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    
+});
+
+function provider(provider_id, north_west_x, north_west_y, south_east_x, south_east_y, zoom)
+{
+    var url = 'http://backend/api/getJson?' +
+       'provider_id=' + provider_id + '&' +
+       'north_west_x=' + north_west_x + '&' +
+       'north_west_y=' + north_west_y + '&' +
+       'south_east_x=' + south_east_x + '&' +
+       'south_east_y=' + south_east_y + '&' +
+       'zoom=' + zoom;
+
+    $.ajax({
+        dataType: "json",
+        url:      url,
+        success:  function(data)
+        {
+            alert(data);
+            /*
+            $.each(data, function(num, provider)
+            {
+
+            });
+            */
+        }
+    });
+}
