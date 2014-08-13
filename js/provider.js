bf428db98d9292406d79bee346a881fcb3ccf189
Igooha <igoooha@gmail.com>
2014-08-13 14:19:32 +0700

    Добавил функцию provider для работы с backend

--- js/provider.js
+++ js/provider.js
@@ -1,29 +1,31 @@
-$(document).ready(function() {
-
-});
-
-function provider(provider_id, north_west_x, north_west_y, south_east_x, south_east_y, zoom)
+/*
+* Функция, которая по двум координатам и провайдеру возвращает
+* JSON массов с данными погоды по каждой метеостанции
+ */
+function provider(provider_id, north_west_x, north_west_y, south_east_x, south_east_y)
 {
-    var url = 'http://backend/api/getJson?' +
+    var host = 'http://10.54.71.69/weather.php?r=weather/find&';
+    var good_host = 'http://10.54.71.69?';
+
+    var good_url = good_host +
        'provider_id=' + provider_id + '&' +
        'north_west_x=' + north_west_x + '&' +
        'north_west_y=' + north_west_y + '&' +
        'south_east_x=' + south_east_x + '&' +
-       'south_east_y=' + south_east_y + '&' +
-       'zoom=' + zoom;
+       'south_east_y=' + south_east_y;
+
+    var url = host +
+        'lon_top=' + north_west_x + '&' +
+        'lat_top=' + north_west_y + '&' +
+        'lon_bottom=' + south_east_x + '&' +
+        'lat_bottom=' + south_east_y;
 
     $.ajax({
-        dataType: "json",
+        dataType: "jsonp",
         url:      url,
         success:  function(data)
         {
-            alert(data);
-            /*
-            $.each(data, function(num, provider)
-            {
-
-            });
-            */
+            // Тут как-то возвращаем массив с данными.
         }
     });
 }
