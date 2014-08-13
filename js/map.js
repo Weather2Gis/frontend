b613ee830e9bf7c59737fe2944dd0b7ead8b49a6
Igooha <igoooha@gmail.com>
2014-08-13 14:23:45 +0700

    Повешал обработчик событий на карту

--- js/map.js
+++ js/map.js
@@ -1,23 +1,17 @@
 DG.then(function () {
-    map = DG.map('map', {
+    var map = DG.map('map', {
         "center": [54.98, 82.89],
         "zoom": 13,
         "fullscreenControl" : false,
         "zoomControl" : false
     });
 
-    //DG.marker([54.93454, 82.78009]).addTo(map);
-    DG.marker([54.93454, 82.99982]).addTo(map);
+    // Вешаем на собития перемещения карты, изменения зума и изменения размера окна подгрузку новых данных
+    map.on('resize movestart viewreset', function (e) {
+        var current_map_position = map.getBounds();
+        var north_west = current_map_position.getNorthWest(); // Получаем координаты левой верхней точки
+        var south_east = current_map_position.getSouthEast(); // Получаем координаты правой нижней точки
 
-
+        var weather_data = provider(1, north_west.lng, north_west.lat, south_east.lng, south_east.lat);
     });
-
-function getMapData()
-{
-    var temp = map.getBounds();
-    return temp.getNorthWest(); // Левая верхняя точка
-    //var b = temp.getSouthEast(); // Правая нижняя точка
-
-
-    // TODO: тут получать данные из карты.
-}
+});
