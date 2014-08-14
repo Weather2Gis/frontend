9a4d55fc6423fe0ec17215a31231599a6148e4a8
Igooha <igoooha@gmail.com>
2014-08-14 13:16:41 +0700

    Настроил кроссдоменный AJAX запрос

--- js/map.js
+++ js/map.js
@@ -1,10 +1,18 @@
 DG.then(function () {
+    var southWest = DG.latLng(80.179, 23.906),
+        northEast = DG.latLng(39.096, 190.898),
+        bounds = DG.latLngBounds(southWest, northEast);
+
     var map = DG.map('map', {
         "center": [54.98, 82.89],
-        "zoom": 13,
+        "zoom": 10,
         "fullscreenControl" : false,
-        "zoomControl" : false
+        "zoomControl" : false,
+        "minZoom" : 4,
+        "maxZoom" : 12,
+        "maxBounds" : bounds
     });
+
 
     // Вешаем на собития перемещения карты, изменения зума и изменения размера окна подгрузку новых данных
     map.on('resize movestart viewreset', function (e) {
@@ -12,6 +20,8 @@ DG.then(function () {
         var north_west = current_map_position.getNorthWest(); // Получаем координаты левой верхней точки
         var south_east = current_map_position.getSouthEast(); // Получаем координаты правой нижней точки
 
-        var weather_data = provider(1, north_west.lng, north_west.lat, south_east.lng, south_east.lat, 1);
+        var weather_data = provider(1, north_west.lng, north_west.lat, south_east.lng, south_east.lat, 1, function(data) {
+            console.log(data);
+        });
     });
 });
--- js/provider.js
+++ js/provider.js
@@ -2,10 +2,11 @@
 * Функция, которая по двум координатам и провайдеру возвращает
 * JSON массов с данными погоды по каждой метеостанции
  */
-function provider(provider_id, north_west_x, north_west_y, south_east_x, south_east_y, zoom)
+function provider(provider_id, north_west_x, north_west_y, south_east_x, south_east_y, zoom, callback)
 {
     var host = 'http://10.54.71.69/weather.php?r=weather/find&';
     var good_host = 'http://10.54.71.69?';
+    var test_host = "http://weather7.apiary-mock.com/api/moscow";
 
     var good_url = good_host +
         'provider_id=' + provider_id + '&' +
@@ -23,10 +24,18 @@ function provider(provider_id, north_west_x, north_west_y, south_east_x, south_e
 
     $.ajax({
         dataType: "jsonp",
-        url:      url,
+        url:      test_host,
+        async:      false,
+        jsonp: false,
+        jsonpCallback: "callback",
         success:  function(data)
         {
-            // Тут как-то возвращаем массив с данными.
+            callback(data);
+        },
+        error: function(data)
+        {
+            callback(data);
         }
     });
+
 }
